04e43d2c3b0b45cf5411eb5d5965f3064115870e
Anastasiia Lytviak <lav21051991@gmail.com>
2022-09-25 20:37:21 +0300

    added BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <BrowserRouter basename="goit-react-hw-08-phonebook">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+              <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
